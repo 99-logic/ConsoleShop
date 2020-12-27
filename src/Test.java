c6e21933157b99622f085ab897cd99aaeb058e4a
lenovo <123>
2020-12-27 10:17:59 +0800

    将输入的用户名和密码与users.xlsx中的信息比较

--- src/Test.java
+++ src/Test.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Test {
@@ -11,5 +12,17 @@ public class Test {
         System.out.println("请输入密码：");
         String password=sc.next();
         System.out.println("你输入的密码："+password);
+
+        File file=new File("C:\\Users\\lenovo\\IdeaProjects\\ConsoleShop\\src\\users.xlsx");
+        ReadExcel readExcel =new ReadExcel();
+        User users[]=readExcel.readExcel(file);
+        for(int i=0;i<users.length;i++){
+            if(username.equals(users[i].getUsername()) && password.equals(users[i].getPassword())){
+                System.out.println("登陆成功");
+                break;
+            }else{
+                System.out.println("登录失败");
+            }
+        }
     }
 }
